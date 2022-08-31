d6a79700b0036a7b61b394d4078b311d2a10ee36
z1nl0x <paulo.kreft@gmail.com>
2022-08-30 21:21:25 -0300

    Labels update

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -82,7 +82,7 @@ const Cart = () => {
                 <div className="item-desc">
                   <div className="flex top">
                     <h5>{item.name}</h5>
-                    <h4>${item.price}</h4>
+                    <h4>R$ {item.price}</h4>
                   </div>
                   <div className="flex bottom">
                     <div>
@@ -124,7 +124,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>${totalPrice}</h3>
+              <h3>R$ {totalPrice}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
